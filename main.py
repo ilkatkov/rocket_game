7e214ea724c239635a61e20b6adc862cfd0c312d
ilkatkov <ilkatkov@yandex.ru>
2021-03-09 18:15:36 +0300

    start project

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+width = 800
+height = 600
+
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Ракета")
+run = True
+
+bg = pygame.transform.scale(pygame.image.load("img/bg.png"), (width, height))
+
+
+while run:
+    window.blit(bg, (0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
